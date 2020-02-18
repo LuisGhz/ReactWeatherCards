cfc3c94785a1c2163f1d55615ae0577f167dd25c
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-02-17 19:38:02 -0600

    Add City and Country style from styled

--- src/components/WeatherCard/location.js
+++ src/components/WeatherCard/location.js
@@ -3,15 +3,26 @@ import styled from '@emotion/styled';
 
 const Location = (props) => 
 {
+    const fontFamily = 'font-family: Merriweather, sans-serif;';
 
     const locationContainer = styled.div`
         text-align:center;
     `
 
+    const City = styled.h1`
+        ${fontFamily}
+        font-size: 1.6rem;
+    `
+
+    const Country = styled.h3`
+        ${fontFamily}
+        font-size: 1.1rem;
+    `
+
     return (
-    <locationContainer >
-        <h1 className='city Merri' >Sydney</h1>
-        <h3 className='country Merri' >AU</h3>
+    <locationContainer>
+        <City>Sydney</City>
+        <Country className='country Merri' >AU</Country>
     </locationContainer>
     );
 }
