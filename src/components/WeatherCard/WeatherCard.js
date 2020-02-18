d3362589e52fe23e04733dddd6ff2296f01ed0cd
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-02-17 20:43:06 -0600

    Include all components

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import styled from '@emotion/styled'
+import React from "react";
+import styled from "@emotion/styled";
 
-import Location from './Location';
+import Location from "./Location";
+import Icon from "./Icon";
+import Condition from "./Condition";
 
-const WeatherCard = (props) => 
-{
-    const red = 200;
-    const Card = styled.div`
-        margin: 0 auto;
-        background: linear-gradient(to bottom, rgba(${red}, 200, 200), pink);
-        width: 200px;
-        height: 240px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        border-radius: 15px;
-    `
+const WeatherCard = props => {
+  const red = 200;
+  const Card = styled.div`
+    margin: 0 auto;
+    background: linear-gradient(to bottom, rgba(${red}, 200, 200), pink);
+    width: 200px;
+    height: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 15px;
+  `;
 
-    return (
-        <Card className='Card'>
-            <Location />
-            <img className='icon' src="./img/Mostly Sunny-2x.png" alt="Wheather icon" />
-            <h1 className='temp Merri' >20 °C</h1>
-            <h3 className='condition Merri' >Clouds</h3>
-        </Card>
-    );
-}
+  return (
+    <Card className="Card">
+      <Location />
+      <Icon />
+      <Condition />
+    </Card>
+  );
+};
 
 export default WeatherCard;
